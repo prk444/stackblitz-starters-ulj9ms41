cc17b0707ab18588c5923fa19ca6292a750b76ee
raghava <prk419918@gmail.com>
2025-02-21 14:04:52 +0530

    kk

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+dotenv.config();
+const port = process.env.PORT;
+
 
 app.use(express.static('static'));
+
+let connect=mongoose.connect("mongodb+srv://poluriraghavas81:gflir33yGuUFolT2@cluster0.30lgl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>console.log("connected to mongodb"))
+.catch((err)=>console.log("error",err));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
